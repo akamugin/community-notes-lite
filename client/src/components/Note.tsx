58f154acc410565b6385bede22b7c64b6fa3332c
akamugin <stella.lee@vanderbilt.edu>
2025-05-12 13:49:08 -0700

    Note updated

--- client/src/components/Note.tsx
+++ client/src/components/Note.tsx
@@ -4,4 +4,11 @@ type NoteProps = {
     postId: number;
 }
 
-export const Note = {{ postId}:}
+const Note = (props: NoteProps) => {
+    
+    return (
+        <div>
+           {`Note for Post #${props.postId}: This is a helpful explanation.`}
+        </div>
+    )
+}
